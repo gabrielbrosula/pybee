cc8299b990216e33e398a62d71c8bff4995489d8
gjrbrosula <82014893+gabrielbrosula@users.noreply.github.com>
2022-08-24 03:37:29 +0800

    Create word validator function

--- spelling_bee.py
+++ spelling_bee.py
@@ -3,22 +3,20 @@ import string
 from english_words import english_words_set
 import keyboard
 
+CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 # Generate a string of 7 letters
-def generateLetters():
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def generate_letters():
     picked_chars = set()
 
     while len(picked_chars) != 7:
         num = random.randint(0, 25)
-        picked_chars.add(chars[num])
-    
-    print(picked_chars)
+        picked_chars.add(CHARS[num])
 
     return list(picked_chars)
 
 # Draw ASCII ART of the characters
-
-def drawLetterHexes(picked_chars):
+def draw_letter_hexes(picked_chars):
     letter_hex_str = """
           ____
          /    \\
@@ -39,6 +37,29 @@ def drawLetterHexes(picked_chars):
 
     # Print the ASCII art string as raw text
     print(r"{}".format(letter_hex_str))
+
+# Check if the word is valid
+def is_word_valid(word, picked_chars):
+
+    word_upper = word.upper()
+
+    # Check if the word contains only valid letters
+    if any(c not in picked_chars for c in word_upper):
+        print("Invalid character(s) present!")
+        return False
+
+    # Check if the word contains the middle letter
+    if (picked_chars[3] not in word_upper):
+        print("Missing center letter!")
+        return False
+
+    # Check if the word is in the word list
+    if (word not in english_words_set):
+        print("Word not in word list!")
+        return False
+    else:
+        print("Valid word!")
+        return True
     
 
 
@@ -47,10 +68,19 @@ def main():
     print("\nPress 'ESC' to quit.\n")
 
     print("The letters are: ")
-    picked_chars = generateLetters()
-    drawLetterHexes(picked_chars)
+    picked_chars = generate_letters()
+    draw_letter_hexes(picked_chars)
 
-    keyboard.wait("esc")
+    # Get user input
+    print("Make words from the letters shown above!")
+
+    while True:
+        word_input = input()
+        is_word_valid(word_input, picked_chars)
+
+        if keyboard.press_and_release('esc'):
+            return
+
 
 if __name__ == "__main__":
     main()
