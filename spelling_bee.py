58d590b9b5498887dc085647f7027a18bf6acb7b
gjrbrosula <82014893+gabrielbrosula@users.noreply.github.com>
2022-08-23 23:24:14 +0800

    Add function to randomly generate letters

--- spelling_bee.py
+++ spelling_bee.py
@@ -1,9 +1,32 @@
+import random
+import string
 from english_words import english_words_set
 import keyboard
 
-print("Welcome to the Python Terminal Spelling Bee!")
+# Generate a string of 7 letters
+def generateLetters() -> set:
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    picked_chars = set()
 
-while True:
-    if keyboard.read_key('q'):
-        break
+    while len(picked_chars) != 7:
+        num = random.randint(0, 25)
+        picked_chars.add(chars[num])
+    
+    print(picked_chars)
+
+    return picked_chars 
+
+
+def main():
+    print("\nWelcome to the PyBee, a Python implementation of New York Times' Spelling Bee!")
+    print("\nPress 'ESC' to quit.\n")
+
+    print("The letters are: ")
+    generateLetters()
+
+    keyboard.wait("esc")
+
+if __name__ == "__main__":
+    main()
+
 
